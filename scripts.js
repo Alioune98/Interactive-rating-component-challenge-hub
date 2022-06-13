b0d725b370118f7780a23ef87deb84b6838c7468
Ali <aliounediagne023@gmail.com>
2022-06-13 16:13:51 -0400

    Rating input and thank you display functionality added, minor css updates

--- scripts.js
+++ scripts.js
@@ -1,7 +1,39 @@
-var btnClicked = document.getElementsByClassName("circle");
+var page = document.body.id;
 
-function getValue(value){
-  alert(value);
+if(page === "rating-state"){
+  document.querySelector(".submit-btn").addEventListener("click", updateRating);
+  document.querySelectorAll(".rating").forEach((value) =>{
+    value.addEventListener("click", function(){
+      localStorage.setItem("rating", this.value);
+      let rating = localStorage.getItem("rating");
+      console.log(rating);
+      this.classList.add("clicked");
+      removeClasses(this);
+    });
+  });
 }
 
-function 
+if(page === "thankyou-state"){
+  let rating = localStorage.getItem("rating");
+  console.log(page);
+  document.querySelector("span").innerHTML = rating;
+  console.log(document.querySelector("span").innerHTML);
+  console.log(rating);
+}
+
+function updateRating(){
+  const rating = localStorage.getItem("rating");
+  if(rating != 0){
+    window.location.href="../Interactive-rating-component-challenge-hub/ThankYouState.html";
+    document.querySelector("span").innerHTML = rating;
+  }
+}
+
+function removeClasses(target){
+  var btns = document.querySelectorAll(".rating");
+  btns.forEach((btn) => {
+    if(btn != target){
+      btn.classList.remove("clicked");
+    }
+  })
+}
